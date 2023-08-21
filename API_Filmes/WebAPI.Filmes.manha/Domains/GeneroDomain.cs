52b5afb8d398c6140f0101c1c3a3e51e805e9421
Pedro H. Alves <leonkene18@gmail.com>
2023-08-21 10:54:03 -0300

    aaa

---
+++ API_Filmes/WebAPI.Filmes.manha/Domains/GeneroDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Filmes.manha.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) Genero
+    /// </summary>
+    public class GeneroDomain
+    {
+       public int IdGenero { get; set; }
+       public string? Nome { get; set; }
+    }
+}
---
+++ API_Filmes/WebAPI.Filmes.manha/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,18 @@
+﻿using WebAPI.Filmes.manha.Domains;
+
+namespace WebAPI.Filmes.manha.Interfaces
+{
+    /// <summary>
+    /// Interface responsavel pelo repositorio GeneroRepository
+    /// Definir os metodos que serao implementados pelo GeneroRepository
+    /// </summary>
+    public interface IGeneroRepository
+    {
+        //tipoRetorno NomeMetodo (TipoParametro nomeParametro)
+
+        void Cadastrar(GeneroDomain novoGenero);
+        List<GeneroDomain> ListarTodos();
+        void AtualizarIdCorpo(GeneroDomain genero);
+        void Deletar(int id);
+    }
+}
