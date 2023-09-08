80106c90949133ba8f0b6f6e1cd2e5ac1835dfe4
Pedro H. Alves <leonkene18@gmail.com>
2023-09-07 21:45:09 -0300

    a

--- API_InLock/senai.inlock.webApi/Controllers/UsuarioController.cs
+++ API_InLock/senai.inlock.webApi/Controllers/UsuarioController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using senai.inlock.webApi.Domains;
 using senai.inlock.webApi.Interfaces;
 using senai.inlock.webApi.Repositories;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace senai.inlock.webApi.Controllers
 {
@@ -17,6 +22,94 @@ namespace senai.inlock.webApi.Controllers
             _usuarioRepository = new UsuarioRepository();
         }
 
+        [HttpGet]
+        public IActionResult ListarTodos()
+        {
+            try
+            {
+                List<Domains.UsuarioDomain> listaUsuario = _usuarioRepository.ListarTodos();
+                return Ok(listaUsuario);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
 
+        [HttpPost]
+        public IActionResult Cadastrar(UsuarioDomain usuario)
+        {
+            try
+            {
+                _usuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Login(UsuarioDomain usuario)
+        {
+            try
+            {
+                UsuarioDomain usuarioBuscado = _usuarioRepository.Login(usuario.Email, usuario.Senha);
+
+                if (usuarioBuscado == null)
+                {
+                    return NotFound("email ou senha invalidos");
+                }
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, usuarioBuscado.IdUsuario.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email),
+                    new Claim(ClaimTypes.Role, usuarioBuscado.IdTipoUsuario.ToString()),
+
+                    new Claim("Claim personalizada","Valor da Claim personalizada")
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("inlock-chave-autenticacao-webapi-dev"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken
+                    (
+                        issuer: "senai.inlock.webApi",
+
+                        audience: "senai.inlock.webApi",
+
+                        claims: claims,
+
+                        expires: DateTime.Now.AddMinutes(5),
+
+                        signingCredentials: creds
+                    );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
     }
 }
