7c57707d16acef14a800daba68f24c62c8334d02
Pedro H. Alves <leonkene18@gmail.com>
2023-09-12 11:02:56 -0300

    a

---
+++ API_inlock.dbFirst/webapi.inlock.dbFirst/Repositories/EstudioRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.inlock.dbFirst.Contexts;
+using webapi.inlock.dbFirst.Domains;
+using webapi.inlock.dbFirst.Interfaces;
+
+namespace webapi.inlock.dbFirst.Repositories
+{
+    public class EstudioRepository : IEstudioRepository
+    {
+        //instancia de uma classe do tipo InlockContext
+        InlockContext ctx = new InlockContext();
+        public void Atualizar(Guid id, Estudio estudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudio BuscarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Estudio novoEstudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudio> Listar()
+        {
+            //Chamaro objeto ctx para executar o metodo de listagem nativo do context dentro do banco de dados
+            return ctx.Estudios.ToList();
+        }
+
+        public List<Estudio> ListarComJogos()
+        {
+            return ctx.Estudios.Include(e => e.Jogos).ToList();
+        }
+    }
+}
