4d8b6657796817a84a4524d67e44ab5650b39c07
Pedro H. Alves <leonkene18@gmail.com>
2023-09-13 10:33:03 -0300

    a

---
+++ API_inlock.dbFirst/webapi.inlock.codeFirst.manha/Domains/Jogo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.inlock.codeFirst.manha.Domains
+{
+    [Table("Jogo")]
+    public class Jogo
+    {
+        [Key]
+        public Guid IdJogo { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Nome do jogo e obrigatorio")]
+        public string? Nome { get; set; }
+
+        [Column(TypeName = "TEXT")]
+        [Required(ErrorMessage = "Descricao obrigatorio")]
+        public int Descricao { get; set; }
+
+        [Column(TypeName = "DATE")]
+        [Required(ErrorMessage = "Data de lancamento obrigatoria")]
+        public DateTime? DataLancamento { get; set; }
+    }
+}
